593160e69ede96d6a55b8f06f9e4cf85da55442a
sgukz <thorapee055@gmail.com>
2021-01-13 20:24:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,7 @@
                         <br>
                         <div class="center-align" id="section_btn_check">
                             <button type="button" id="btn_check_in" onclick="checkIn()" class="waves-effect waves-light btn green accent-4"><b>Check-in</b></button>
-                            <button type="button" id="btn_check_out" class="waves-effect waves-light btn red darken-3"><b>Check-Out</b></button>
+                            <button type="button" id="btn_check_out" onclick="checkOut()" class="waves-effect waves-light btn red darken-3"><b>Check-Out</b></button>
                         </div>
                     </div>
                 </div>
@@ -79,125 +79,193 @@
 <script src="https://static.line-scdn.net/liff/edge/2.1/liff.js"></script>
 <script>
     let meeting_id = $("#docno").val();
-    // let userID = $("#userId").val();
     let base_url = "https://service-api-1meeting.herokuapp.com";
     let today = new Date().getTime();
-    function checkIn(){
+
+    function checkIn() {
         liff
             .getProfile()
             .then((profile) => {
                 const userId = profile.userId;
-                alert(userId);
-                alert(meeting_id);
+                let is_check = 1;
+                let data = {
+                    userId,
+                    meeting_id,
+                    is_check,
+                };
+                $.ajax({
+                        method: "POST",
+                        url: `${base_url}/saveCheckin`,
+                        data: data
+                    })
+                    .done((resp) => {
+                        let data = resp
+                        if (data.code === 200) {
+                            Swal.fire({
+                                'icon': "success",
+                                'title': data.msg,
+                                'text': "เรียบร้อย",
+                                'showConfirmButton': false,
+                            })
+                            setTimeout(() => {
+                                window.location = `?docno=${data.data}`;
+                            }, 1000)
+                        }
+                    })
+                    .fail((error) => {
+                        console.log(error);
+                    })
+            });
+    }
+
+    function checkOut() {
+        liff
+            .getProfile()
+            .then((profile) => {
+                const userId = profile.userId;
+                let is_check = 2;
+                let data = {
+                    userId,
+                    meeting_id,
+                    is_check,
+                };
+                $.ajax({
+                        method: "POST",
+                        url: `${base_url}/saveCheckin`,
+                        data: data
+                    })
+                    .done((resp) => {
+                        let data = resp
+                        if (data.code === 200) {
+                            Swal.fire({
+                                'icon': "success",
+                                'title': data.msg,
+                                'text': "เรียบร้อย",
+                                'showConfirmButton': false,
+                            })
+                            setTimeout(() => {
+                                window.location = `?docno=${data.data}`;
+                            }, 1000)
+                        }
+                    })
+                    .fail((error) => {
+                        console.log(error);
+                    })
             });
     }
     // $("#btn_check_in").click(function() {
     //     saveCheckin(userID, meeting_id, 1);
     // });
 
-    $("#btn_check_out").click(function() {
-        saveCheckin(userID, meeting_id, 2);
-    });
+    // $("#btn_check_out").click(function() {
+    //     saveCheckin(userID, meeting_id, 2);
+    // });
 
-    function saveCheckin(userId, docno, is_check) {
-        let data = {
-            userId,
-            docno,
-            is_check,
-        };
-        $.ajax({
-                method: "POST",
-                url: `${base_url}/saveCheckin`,
-                data: data
-            })
-            .done((resp) => {
-                let data = resp
-                if (data.code === 200) {
-                    Swal.fire({
-                        'icon': "success",
-                        'title': data.msg,
-                        'text': "เรียบร้อย",
-                        'showConfirmButton': false,
-                    })
-                    setTimeout(() => {
-                        window.location = `?docno=${data.data}`;
-                    }, 1000)
-                }
-            })
-            .fail((error) => {
-                console.log(error);
-            })
-    }
-
-    // $.ajax({
-    //         method: "GET",
-    //         url: `${base_url}/getMeetingByDocno/${meeting_id}`,
-    //         data: ""
-    //     })
-    //     .done((resp) => {
-    //         // console.log(resp);
-    //         if (resp.code === 200) {
-    //             $("#loading").addClass("hidden");
-    //             let data = resp.data[0]
-    //             let endDate = new Date(data.end_date).getTime();
-    //             if (today > endDate) {
-    //                 $("#btn_check_in").addClass("hidden");
-    //                 $("#btn_check_out").addClass("hidden");
-    //                 $("#is_check_in").html(`<span class='red-text'><b>จบการประชุมแล้ว</b></span>`);
-    //                 $("#is_check_out").html(`<span></span>`);
+    // function saveCheckin(userId, docno, is_check) {
+    //     let data = {
+    //         userId,
+    //         docno,
+    //         is_check,
+    //     };
+    //     $.ajax({
+    //             method: "POST",
+    //             url: `${base_url}/saveCheckin`,
+    //             data: data
+    //         })
+    //         .done((resp) => {
+    //             let data = resp
+    //             if (data.code === 200) {
+    //                 Swal.fire({
+    //                     'icon': "success",
+    //                     'title': data.msg,
+    //                     'text': "เรียบร้อย",
+    //                     'showConfirmButton': false,
+    //                 })
+    //                 setTimeout(() => {
+    //                     window.location = `?docno=${data.data}`;
+    //                 }, 1000)
     //             }
-    //             $("#section-scan").addClass("hidden");
-    //             $("#lebel-scan").addClass("hidden");
-    //             $("#section-data").removeClass("hidden");
-    //             let meeting_name = `<strong style='font-size: 16px;'><b>หัวข้อ : ${data.meeting_name}</b></strong>`;
-    //             let meeting_date = `<strong>ระหว่างวันที่ <b>${(formateDate(data.start_date) === formateDate(data.end_date) ? formateDate(data.start_date) : formateDate(data.start_date) + " - " + formateDate(data.end_date))}</b></strong>`;
-    //             let meeting_time = `<strong>เริ่มประชุมเวลา <b> ${formateTime(data.start_date)} - ${formateTime(data.end_date)}</b></strong>`;
-    //             let meeting_total = `<strong><b>เข้าประชุมแล้ว ${resp.total[0].total_meeting}/${data.human_amount} คน</b></strong>`;
-    //             $("#meeting_total").html(meeting_total);
-    //             $("#meeting_name").html(meeting_name);
-    //             $("#meeting_date").html(meeting_date);
-    //             $("#meeting_time").html(meeting_time);
-    //             $.ajax({
-    //                     method: "GET",
-    //                     url: `${base_url}/getMeetingRegisByUserID/${meeting_id}/${userID}`,
-    //                     data: ""
-    //                 })
-    //                 .done((resp) => {
-    //                     // console.log(resp);
-    //                     let dataRegis = (resp.data !== null) ? resp.data[0] : ""
-    //                     if (dataRegis !== "") {
-    //                         let meeting_total = `<strong><b>เข้าประชุมแล้ว ${dataRegis.cntMeeting}/${data.human_amount} คน</b></strong>`;
-    //                         $("#meeting_total").html(meeting_total);
-    //                         if (dataRegis.check_in_date !== null) {
-    //                             $("#btn_check_in").addClass("hidden");
-    //                             if (dataRegis.check_out_date !== null) {
-    //                                 $("#btn_check_out").addClass("hidden");
-    //                                 let is_check_out = `<span class="grey lighten-3 red-text" id="check_in_date">ออกประชุมเมื่อ<br> 
-    //                                 วันที่ ${formateDate(dataRegis.check_out_date)} เวลา ${formateTime(dataRegis.check_out_date)}</span>`;
-    //                                 $("#is_check_out").html(is_check_out);
-    //                             }
-    //                             // $("#btn_check_out").removeClass("hidden");
-    //                             let is_check_in = `<span class="grey lighten-3 green-text" id="check_in_date">เข้าร่วมประชุมเมื่อ<br> 
-    //                                 วันที่ ${formateDate(dataRegis.check_in_date)} เวลา ${formateTime(dataRegis.check_in_date)}</span>`;
-    //                             $("#is_check_in").html(is_check_in);
-    //                         }
-    //                     }
-    //                 })
-    //         } else {
-    //             $("#loading").removeClass("hidden");
-    //             Swal.fire({
-    //                 icon: 'error',
-    //                 title: 'ไม่พบข้อมูล...',
-    //                 text: 'กรุณาลองใหม่อีกครั้ง',
-    //             })
-    //         }
-    //     })
-
-
+    //         })
+    //         .fail((error) => {
+    //             console.log(error);
+    //         })
+    // }
+    function loadData() {
+        liff
+            .getProfile()
+            .then((profile) => {
+                const userId = profile.userId;
+                $.ajax({
+                        method: "GET",
+                        url: `${base_url}/getMeetingByDocno/${meeting_id}`,
+                        data: ""
+                    })
+                    .done((resp) => {
+                        // console.log(resp);
+                        if (resp.code === 200) {
+                            $("#loading").addClass("hidden");
+                            let data = resp.data[0]
+                            let endDate = new Date(data.end_date).getTime();
+                            if (today > endDate) {
+                                $("#btn_check_in").addClass("hidden");
+                                $("#btn_check_out").addClass("hidden");
+                                $("#is_check_in").html(`<span class='red-text'><b>จบการประชุมแล้ว</b></span>`);
+                                $("#is_check_out").html(`<span></span>`);
+                            }
+                            $("#section-scan").addClass("hidden");
+                            $("#lebel-scan").addClass("hidden");
+                            $("#section-data").removeClass("hidden");
+                            let meeting_name = `<strong style='font-size: 16px;'><b>หัวข้อ : ${data.meeting_name}</b></strong>`;
+                            let meeting_date = `<strong>ระหว่างวันที่ <b>${(formateDate(data.start_date) === formateDate(data.end_date) ? formateDate(data.start_date) : formateDate(data.start_date) + " - " + formateDate(data.end_date))}</b></strong>`;
+                            let meeting_time = `<strong>เริ่มประชุมเวลา <b> ${formateTime(data.start_date)} - ${formateTime(data.end_date)}</b></strong>`;
+                            let meeting_total = `<strong><b>เข้าประชุมแล้ว ${resp.total[0].total_meeting}/${data.human_amount} คน</b></strong>`;
+                            $("#meeting_total").html(meeting_total);
+                            $("#meeting_name").html(meeting_name);
+                            $("#meeting_date").html(meeting_date);
+                            $("#meeting_time").html(meeting_time);
+                            $.ajax({
+                                    method: "GET",
+                                    url: `${base_url}/getMeetingRegisByUserID/${meeting_id}/${userId}`,
+                                    data: ""
+                                })
+                                .done((resp) => {
+                                    // console.log(resp);
+                                    let dataRegis = (resp.data !== null) ? resp.data[0] : ""
+                                    if (dataRegis !== "") {
+                                        let meeting_total = `<strong><b>เข้าประชุมแล้ว ${dataRegis.cntMeeting}/${data.human_amount} คน</b></strong>`;
+                                        $("#meeting_total").html(meeting_total);
+                                        if (dataRegis.check_in_date !== null) {
+                                            $("#btn_check_in").addClass("hidden");
+                                            if (dataRegis.check_out_date !== null) {
+                                                $("#btn_check_out").addClass("hidden");
+                                                let is_check_out = `<span class="grey lighten-3 red-text" id="check_in_date">ออกประชุมเมื่อ<br> 
+                                    วันที่ ${formateDate(dataRegis.check_out_date)} เวลา ${formateTime(dataRegis.check_out_date)}</span>`;
+                                                $("#is_check_out").html(is_check_out);
+                                            }
+                                            // $("#btn_check_out").removeClass("hidden");
+                                            let is_check_in = `<span class="grey lighten-3 green-text" id="check_in_date">เข้าร่วมประชุมเมื่อ<br> 
+                                    วันที่ ${formateDate(dataRegis.check_in_date)} เวลา ${formateTime(dataRegis.check_in_date)}</span>`;
+                                            $("#is_check_in").html(is_check_in);
+                                        }
+                                    }
+                                })
+                        } else {
+                            $("#loading").removeClass("hidden");
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'ไม่พบข้อมูล...',
+                                text: 'กรุณาลองใหม่อีกครั้ง',
+                            })
+                        }
+                    })
+            });
+    }
     liff.init({
             liffId: "1655384297-Y7egqg67",
         },
-        () => {},
+        () => {
+            loadData();
+        },
         (err) => alert(error.message)
 
     );
